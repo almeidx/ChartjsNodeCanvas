2bbff7f4cbbb80aad96a4cc2e48b2335d1db2030
almeidx <almeidx@pm.me>
2023-02-21 18:02:30 +0000

    use @napi-rs/canvas

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Readable } from 'stream';
-import { Chart as ChartJS, ChartConfiguration, ChartComponentLike } from 'chart.js';
-import { createCanvas, registerFont, Image } from 'canvas';
-import { freshRequire } from './freshRequire';
-import { BackgroundColourPlugin } from './backgroundColourPlugin';
+import {
+	Chart as ChartJS,
+	ChartConfiguration,
+	ChartComponentLike,
+} from 'chart.js';
+import { createCanvas, GlobalFonts, Image } from '@napi-rs/canvas';
+import { BackgroundColourPlugin } from './backgroundColourPlugin.js';
+import { Canvas } from '@napi-rs/canvas';
 
 export type ChartJSNodeCanvasPlugins = {
 	/**
@@ -25,15 +29,6 @@ export type ChartJSNodeCanvasPlugins = {
 export type ChartCallback = (chartJS: typeof ChartJS) => void | Promise<void>;
 export type CanvasType = 'pdf' | 'svg';
 export type MimeType = 'image/png' | 'image/jpeg';
-
-// https://github.com/Automattic/node-canvas#non-standard-apis
-type Canvas	= HTMLCanvasElement & {
-	toBuffer(callback: (err: Error|null, result: Buffer) => void, mimeType?: string, config?: any): void;
-	toBuffer(mimeType?: string, config?: any): Buffer;
-	createPNGStream(config?: any): Readable;
-	createJPEGStream(config?: any): Readable;
-	createPDFStream(config?: any): Readable;
-};
 
 export interface ChartJSNodeCanvasOptions {
 	/**
@@ -64,40 +59,32 @@ export interface ChartJSNodeCanvasOptions {
 }
 
 export class ChartJSNodeCanvas {
-
 	private readonly _width: number;
 	private readonly _height: number;
-	private readonly _chartJs: typeof ChartJS;
+	private _chartJs!: typeof ChartJS;
 	private readonly _createCanvas: typeof createCanvas;
-	private readonly _registerFont: typeof registerFont;
 	private readonly _image: typeof Image;
-	private readonly _type?: CanvasType;
 
 	/**
 	 * Create a new instance of CanvasRenderService.
 	 *
 	 * @param options Configuration for this instance
 	 */
-	constructor(options: ChartJSNodeCanvasOptions) {
-
-		if (options === null || typeof (options) !== 'object') {
+	constructor(public options: ChartJSNodeCanvasOptions) {
+		if (options === null || typeof options !== 'object') {
 			throw new Error('An options parameter object is required');
 		}
-		if (!options.width || typeof (options.width) !== 'number') {
+		if (!options.width || typeof options.width !== 'number') {
 			throw new Error('A width option is required');
 		}
-		if (!options.height || typeof (options.height) !== 'number') {
+		if (!options.height || typeof options.height !== 'number') {
 			throw new Error('A height option is required');
 		}
 
 		this._width = options.width;
 		this._height = options.height;
-		const canvas = freshRequire('canvas');
-		this._createCanvas = canvas.createCanvas;
-		this._registerFont = canvas.registerFont;
-		this._image = canvas.Image;
-		this._type = options.type && options.type.toLowerCase() as CanvasType;
-		this._chartJs = this.initialize(options);
+		this._createCanvas = createCanvas;
+		this._image = Image;
 	}
 
 	/**
@@ -107,21 +94,18 @@ export class ChartJSNodeCanvas {
 	 * @param configuration The Chart JS configuration for the chart to render.
 	 * @param mimeType The image format, `image/png` or `image/jpeg`.
 	 */
-	public renderToDataURL(configuration: ChartConfiguration, mimeType: MimeType = 'image/png'): Promise<string> {
-
+	public renderToDataURL(
+		configuration: ChartConfiguration,
+		mimeType: MimeType = 'image/png'
+	): Promise<string> {
 		const chart = this.renderChart(configuration);
 		return new Promise<string>((resolve, reject) => {
 			if (!chart.canvas) {
 				return reject(new Error('Canvas is null'));
 			}
-			const canvas = chart.canvas as Canvas;
-			canvas.toDataURL(mimeType, (error: Error | null, png: string) => {
-				chart.destroy();
-				if (error) {
-					return reject(error);
-				}
-				return resolve(png);
-			});
+			const dataURL = chart.canvas.toDataURL(mimeType);
+			chart.destroy();
+			resolve(dataURL);
 		});
 	}
 
@@ -132,12 +116,15 @@ export class ChartJSNodeCanvas {
 	 * @param configuration The Chart JS configuration for the chart to render.
 	 * @param mimeType The image format, `image/png` or `image/jpeg`.
 	 */
-	public renderToDataURLSync(configuration: ChartConfiguration, mimeType: MimeType = 'image/png'): string {
-
+	public renderToDataURLSync(
+		configuration: ChartConfiguration,
+		mimeType: MimeType = 'image/png'
+	): string {
 		const chart = this.renderChart(configuration);
 		if (!chart.canvas) {
 			throw new Error('Canvas is null');
 		}
+		// @ts-expect-error
 		const canvas = chart.canvas as Canvas;
 		const dataUrl = canvas.toDataURL(mimeType);
 		chart.destroy();
@@ -151,21 +138,21 @@ export class ChartJSNodeCanvas {
 	 * @param configuration The Chart JS configuration for the chart to render.
 	 * @param mimeType A string indicating the image format. Valid options are `image/png`, `image/jpeg` (if node-canvas was built with JPEG support) or `raw` (unencoded ARGB32 data in native-endian byte order, top-to-bottom). Defaults to `image/png` for image canvases, or the corresponding type for PDF or SVG canvas.
 	 */
-	public renderToBuffer(configuration: ChartConfiguration, mimeType: MimeType = 'image/png'): Promise<Buffer> {
-
+	public renderToBuffer(
+		configuration: ChartConfiguration,
+		mimeType: MimeType = 'image/png'
+	): Promise<Buffer> {
 		const chart = this.renderChart(configuration);
 		return new Promise<Buffer>((resolve, reject) => {
 			if (!chart.canvas) {
-				throw new Error('Canvas is null');
+				reject(new Error('Canvas is null'));
 			}
+			// @ts-expect-error
 			const canvas = chart.canvas as Canvas;
-			canvas.toBuffer((error: Error | null, buffer: Buffer) => {
-				chart.destroy();
-				if (error) {
-					return reject(error);
-				}
-				return resolve(buffer);
-			}, mimeType);
+			// @ts-expect-error
+			const buf = canvas.toBuffer(mimeType);
+			chart.destroy();
+			resolve(buf);
 		});
 	}
 
@@ -176,73 +163,70 @@ export class ChartJSNodeCanvas {
 	 * @param configuration The Chart JS configuration for the chart to render.
 	 * @param mimeType A string indicating the image format. Valid options are `image/png`, `image/jpeg` (if node-canvas was built with JPEG support), `raw` (unencoded ARGB32 data in native-endian byte order, top-to-bottom), `application/pdf` (for PDF canvases) and image/svg+xml (for SVG canvases). Defaults to `image/png` for image canvases, or the corresponding type for PDF or SVG canvas.
 	 */
-	public renderToBufferSync(configuration: ChartConfiguration, mimeType: MimeType | 'application/pdf' | 'image/svg+xml' = 'image/png'): Buffer {
-
+	public renderToBufferSync(
+		configuration: ChartConfiguration,
+		mimeType: MimeType = 'image/png'
+	): Buffer {
 		const chart = this.renderChart(configuration);
 		if (!chart.canvas) {
 			throw new Error('Canvas is null');
 		}
+		// @ts-expect-error
 		const canvas = chart.canvas as Canvas;
-		const buffer =  canvas.toBuffer(mimeType);
+		// @ts-expect-error
+		const buffer = canvas.toBuffer(mimeType);
 		chart.destroy();
 		return buffer;
 	}
 
 	/**
+	 * THIS DOES NOT WORK.
+	 *
 	 * Render to a stream.
 	 * @see https://github.com/Automattic/node-canvas#canvascreatepngstream
 	 *
 	 * @param configuration The Chart JS configuration for the chart to render.
 	 * @param mimeType A string indicating the image format. Valid options are `image/png`, `image/jpeg` (if node-canvas was built with JPEG support), `application/pdf` (for PDF canvases) and image/svg+xml (for SVG canvases). Defaults to `image/png` for image canvases, or the corresponding type for PDF or SVG canvas.
 	 */
-	public renderToStream(configuration: ChartConfiguration, mimeType: MimeType | 'application/pdf' = 'image/png'): Readable {
-
+	public renderToStream(
+		configuration: ChartConfiguration,
+		mimeType: MimeType = 'image/png'
+	): Readable {
 		const chart = this.renderChart(configuration);
 		if (!chart.canvas) {
 			throw new Error('Canvas is null');
 		}
+		// @ts-expect-error
 		const canvas = chart.canvas as Canvas;
 		setImmediate(() => chart.destroy());
 		switch (mimeType) {
 			case 'image/png':
+				// @ts-expect-error
 				return canvas.createPNGStream();
 			case 'image/jpeg':
+				// @ts-expect-error
 				return canvas.createJPEGStream();
-			case 'application/pdf':
-				return canvas.createPDFStream();
 			default:
 				throw new Error(`Un-handled mimeType: ${mimeType}`);
 		}
 	}
 
-	/**
-	 * Use to register the font with Canvas to use a font file that is not installed as a system font, this must be done before the Canvas is created.
-	 *
-	 * @param path The path to the font file.
-	 * @param options The font options.
-	 * @example
-	 * registerFont('comicsans.ttf', { family: 'Comic Sans' });
-	 */
-	public registerFont(path: string, options: { readonly family: string, readonly weight?: string, readonly style?: string }): void {
-
-		this._registerFont(path, options);
-	}
-
-	private initialize(options: ChartJSNodeCanvasOptions): typeof ChartJS {
-
-		const chartJs: typeof ChartJS = require('chart.js');
+	public async initialize(
+		options: ChartJSNodeCanvasOptions
+	): Promise<typeof ChartJS> {
+		const chartJs = (await import('chart.js')).default.Chart;
+		this._chartJs = chartJs;
 
 		if (options.plugins?.requireChartJSLegacy) {
 			for (const plugin of options.plugins.requireChartJSLegacy) {
-				require(plugin);
-				delete require.cache[require.resolve(plugin)];
+				await import(plugin);
 			}
 		}
 
 		if (options.plugins?.globalVariableLegacy) {
 			(global as any).Chart = chartJs;
 			for (const plugin of options.plugins.globalVariableLegacy) {
-				freshRequire(plugin);
+				await import(plugin);
 			}
 			delete (global as any).Chart;
 		}
@@ -250,7 +234,8 @@ export class ChartJSNodeCanvas {
 		if (options.plugins?.modern) {
 			for (const plugin of options.plugins.modern) {
 				if (typeof plugin === 'string') {
-					chartJs.register(freshRequire(plugin));
+					const mod = await import(plugin);
+					chartJs.register(mod.default ?? mod);
 				} else {
 					chartJs.register(plugin);
 				}
@@ -259,7 +244,8 @@ export class ChartJSNodeCanvas {
 
 		if (options.plugins?.requireLegacy) {
 			for (const plugin of options.plugins.requireLegacy) {
-				chartJs.register(freshRequire(plugin));
+				const mod = await import(plugin);
+				chartJs.register(mod.default ?? mod);
 			}
 		}
 
@@ -268,24 +254,28 @@ export class ChartJSNodeCanvas {
 		}
 
 		if (options.backgroundColour) {
-			chartJs.register(new BackgroundColourPlugin(options.width, options.height, options.backgroundColour));
+			chartJs.register(
+				new BackgroundColourPlugin(
+					options.width,
+					options.height,
+					options.backgroundColour
+				)
+			);
 		}
-
-		delete require.cache[require.resolve('chart.js')];
 
 		return chartJs;
 	}
 
 	private renderChart(configuration: ChartConfiguration): ChartJS {
-
-		const canvas = this._createCanvas(this._width, this._height, this._type);
-		(canvas as any).style = (canvas as any).style || {};
+		const canvas = this._createCanvas(this._width, this._height);
+		(canvas as any).style ||= {};
 		// Disable animation (otherwise charts will throw exceptions)
-		configuration.options = configuration.options || {};
+		configuration.options ||= {};
 		configuration.options.responsive = false;
-		configuration.options.animation = false as any;
+		configuration.options.animation = false;
 		const context = canvas.getContext('2d');
 		(global as any).Image = this._image; // Some plugins use this API
+		// @ts-expect-error
 		const chart = new this._chartJs(context, configuration);
 		delete (global as any).Image;
 		return chart;
